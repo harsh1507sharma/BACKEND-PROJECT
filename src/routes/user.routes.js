7c0eb144ea9ced8f562fbdc6f4c7c3a8133af222
HARSH SHARMA <hs0797227@gmail.com>
2025-12-24 12:23:05 +0530

    MINor change

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,12 @@ import {upload} from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {name : "avatar", maxCount : 1},
+        {name : "coverimage", maxCount : 1}
+
+    ]),
+    registerUser)
 
 export default router;
